122104066e461348af727351b047a77cf9ef6784
Edgar Ruiz <edgararuiz@users.noreply.github.com>
2017-12-31 20:57:19 -0600

    Initial blogdown draft

---
+++ tools/blogdown/create_site.R
@@ -0,0 +1,31 @@
+blogdown_build <- function(skip_theme = TRUE) {
+  
+  if (file.exists("content")) unlink("content", recursive = TRUE)
+  if (file.exists("static"))  unlink("static", recursive = TRUE)
+  if (file.exists("public"))  unlink("public", recursive = TRUE)
+  
+  if(!file.exists("content")) dir.create("content")
+  if(!file.exists("content/reference")) dir.create("content/reference")
+  if(!file.exists("static")) dir.create("static")
+  if(!file.exists("themes")) dir.create("themes")
+
+  file.copy("vignettes/", "content/", recursive = TRUE)
+  file.copy("README.Rmd", "content/index.Rmd")
+
+  current_theme <- list.files("tools/blogdown/themes/")[1]
+  theme_target <- paste0("themes/", current_theme )
+  if (!skip_theme &&  file.exists(theme_target)) {
+    if (file.exists(theme_target)) unlink(theme_target, recursive = TRUE)
+    
+    file.copy(
+      paste0("tools/blogdown/themes/", current_theme), 
+      "themes/", 
+      recursive = TRUE
+      )
+    
+  }
+  
+  
+  pkgdown::build_reference(path = "content/reference")
+  blogdown::serve_site()
+}
