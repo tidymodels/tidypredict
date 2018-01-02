ab50d06eb2620c2cf402739a6ac98215c8806e60
edgararuiz <edgararuiz@gmail.com>
2018-01-02 08:15:51 -0600

    Removed print reference pages

--- tools/blogdown/create_site.R
+++ tools/blogdown/create_site.R
@@ -1,3 +1,4 @@
+library(purrr)
 blogdown_build <- function() {
   
   if (file.exists("content")) unlink("content", recursive = TRUE)
@@ -7,10 +8,17 @@ blogdown_build <- function() {
   
   if(!file.exists("content")) dir.create("content")
   if(!file.exists("content/reference")) dir.create("content/reference")
+  if(!file.exists("content/articles")) dir.create("content/articles")
   if(!file.exists("static")) dir.create("static")
   if(!file.exists("layouts")) dir.create("layouts")
 
-  file.copy("vignettes/", "content/", recursive = TRUE)
+  contents <- list.files("vignettes")
+  three_last <- substr(contents, nchar(contents) - 3, nchar(contents))
+  contents <- contents[three_last == ".Rmd"]
+  
+  contents %>%
+    map(~file.copy(file.path("vignettes", .x),
+                   "content/articles/"))
   
   file.copy("README.Rmd", "content/index.Rmd")
 
@@ -29,3 +37,5 @@ blogdown_build <- function() {
   pkgdown::build_reference(path = "content/reference")
   blogdown::serve_site()
 }
+
+
