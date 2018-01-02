0aff4c399815c95ecebc5f7762dc63af01285a68
edgararuiz <edgararuiz@gmail.com>
2018-01-01 20:04:37 -0600

    Remove theme depedency

--- tools/blogdown/create_site.R
+++ tools/blogdown/create_site.R
@@ -1,33 +1,30 @@
-blogdown_build <- function(skip_theme = TRUE) {
+blogdown_build <- function() {
   
   if (file.exists("content")) unlink("content", recursive = TRUE)
   if (file.exists("static"))  unlink("static", recursive = TRUE)
+  if (file.exists("layouts"))  unlink("layouts", recursive = TRUE)
   if (file.exists("public"))  unlink("public", recursive = TRUE)
   
   if(!file.exists("content")) dir.create("content")
   if(!file.exists("content/reference")) dir.create("content/reference")
   if(!file.exists("static")) dir.create("static")
-  if(!file.exists("themes")) dir.create("themes")
+  if(!file.exists("layouts")) dir.create("layouts")
 
   file.copy("vignettes/", "content/", recursive = TRUE)
+  
   file.copy("README.Rmd", "content/index.Rmd")
 
-  current_theme <- list.files("tools/blogdown/themes/")[1]
-  theme_target <- paste0("themes/", current_theme )
-  if (!skip_theme) {
-    if (file.exists(theme_target)) unlink(theme_target, recursive = TRUE)
-    file.copy(
-      paste0("tools/blogdown/themes/", current_theme), 
-      "themes/", 
-      recursive = TRUE
-    )}
+  file.copy(
+    paste0("tools/blogdown/layouts/"), 
+    rprojroot::find_rstudio_root_file(), 
+    recursive = TRUE
+  )
   
-  if(!file.exists(theme_target)){
-    file.copy(
-      paste0("tools/blogdown/themes/", current_theme), 
-      "themes/", 
-      recursive = TRUE
-    )}
+  file.copy(
+    paste0("tools/blogdown/static/"), 
+    rprojroot::find_rstudio_root_file(), 
+    recursive = TRUE
+  )
   
   pkgdown::build_reference(path = "content/reference")
   blogdown::serve_site()
