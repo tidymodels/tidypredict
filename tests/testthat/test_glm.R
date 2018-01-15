b0a51581575141ba26638bdea9fe1890e2a19773
edgararuiz <edgararuiz@gmail.com>
2018-01-15 08:57:00 -0600

    Prep to merge PR

--- tests/testthat/test_glm.R
+++ tests/testthat/test_glm.R
@@ -15,9 +15,3 @@ test_that("Individual prediction difference is never above 1e-12", {
   expect_false(has_alert( glm( am ~ wt + cyl,        data = df, family = "binomial")))
   expect_false(has_alert( glm( am ~ wt + disp + cyl, data = df, family = "binomial")))
 })
- 
-mfail <- glm(am ~ wt + cyl, data = df, family = "quasibinomial")
-test_that("Fails when model is not guassian or binomial", {
- expect_error(tidypredict_fit(mfail))
-})
-
