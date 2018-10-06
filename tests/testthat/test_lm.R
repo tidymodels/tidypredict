4ffd6ee78dc1b0a1e9efaa8e71592f12c766b27b
Edgar Ruiz <edgararuiz@users.noreply.github.com>
2018-10-06 13:27:29 -0500

    Simplifies tests

--- tests/testthat/test_lm.R
+++ tests/testthat/test_lm.R
@@ -3,21 +3,7 @@ context("lm")
 df <- mtcars
 df$cyl <- paste0("cyl", df$cyl)
 
-has_alert <- function(model) {
-  # test1: check for any predictions are above the threshold
-  test1 <- tidypredict_test(
-    model,
-    include_intervals = TRUE
-  )$alert
-  # test2: to see if the if the same results are returned
-  # when a parsed model is passed instead of a model
-  pm <- parse_model(model)
-  test_pm <- df %>% tidypredict_to_column(pm)
-  test_original <- df %>% tidypredict_to_column(model)
-  test2 <- test_pm != test_original
-
-  any(test1, test2)
-}
+has_alert <- function(...) tidypredict_test(...)$alert
 
 test_that("Predictions within threshold and parsed model results are equal", {
   expect_false(has_alert(lm(mpg ~ wt, offset = am, data = df)))
