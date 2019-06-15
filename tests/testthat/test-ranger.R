401bb5fb3f63f1518534268c9cae768df755821d
edgararuiz <edgararuiz@gmail.com>
2019-06-15 16:12:58 -0500

    Adds rf tests, corrects parsed ranger label

--- tests/testthat/test-ranger.R
+++ tests/testthat/test-ranger.R
@@ -1,22 +1,22 @@
 context("ranger")
 set.seed(100)
 rf_model <- ranger::ranger(Species ~ ., data = iris, num.trees = 100)
+tf <- tidypredict_fit(rf_model)
+pm <- parse_model(rf_model)
 
-test_that("Returns the correct type", {
-  expect_is(parse_model(rf_model), "list")
-  #expect_is(tidypredict_sql(rf_model, dbplyr::simulate_dbi()), "sql")
-  expect_is(tidypredict_fit(rf_model), "list")
+
+test_that("Returns the correct type and dimensions", {
+  expect_is(pm, "list")
+  expect_equal(length(pm), 2)
+  expect_equal(length(pm$trees), 100)
+  expect_equal(pm$general$model, "randomForest")
+  expect_equal(pm$general$version, 2)
 })
 
-# test_that("All tests are under the threshold (5)", {
-#   expect_false(tidypredict_test(rf_model, threshold = 5, df = iris)$alert)
-# })
-# 
-# 
-# pm <- parse_model(rf_model)
-# test_pm <- iris %>% tidypredict_to_column(pm)
-# test_original <- iris %>% tidypredict_to_column(rf_model)
-# 
-# test_that("Parsed model returns same results as model", {
-#   expect_equal(test_pm, test_original)
-# })
+test_that("Returns expected case_when() dplyr formula", {
+  expect_equal(
+    rlang::expr_text(tf[[1]]),
+    "case_when(Petal.Length < 2.45 & Petal.Width < 1.75 ~ \"setosa\", \n    Petal.Length < 4.85 & Petal.Width >= 1.75 ~ \"virginica\", \n    Petal.Length >= 4.85 & Petal.Width >= 1.75 ~ \"virginica\", \n    Petal.Length < 5.4 & Petal.Length >= 2.45 & Petal.Width < \n        1.75 ~ \"versicolor\", Petal.Length >= 5.4 & Petal.Length >= \n        2.45 & Petal.Width < 1.75 ~ \"virginica\")"
+  )
+})
+
