b629775d1936e99f59d0e4821c29f43ae414ec96
Jiaxiang Li <alex.lijiaxiang@foxmail.com>
2020-07-13 11:51:41 +0800

    add test for LR xgb model
    update https://github.com/tidymodels/tidypredict/pull/66#discussion_r453384901

--- tests/testthat/test_xgboost.R
+++ tests/testthat/test_xgboost.R
@@ -56,3 +56,64 @@ test_that("Model can be saved and re-loaded", {
   pm <- as_parsed_model(l)
   expect_silent(tidypredict_fit(pm))
 })
+
+base_score <- mean(mtcars$am)
+xgb_reglogistic_basescore <-
+  xgboost::xgb.train(
+    params = list(
+      max_depth = 2,
+      silent = 1,
+      objective = "reg:logistic",
+      base_score = base_score
+    ),
+    data = xgb_bin_data,
+    nrounds = 4
+  )
+xgb_binarylogistic_basescore <-
+  xgboost::xgb.train(
+    params = list(
+      max_depth = 2,
+      silent = 1,
+      objective = "binary:logistic",
+      base_score = base_score
+    ),
+    data = xgb_bin_data,
+    nrounds = 4
+  )
+
+parsedmodel_xgb_reglogistic_basescore <- parse_model(xgb_reglogistic_basescore)
+parsedmodel_xgb_binarylogistic_basescore <- parse_model(xgb_binarylogistic_basescore)
+
+test_that("Base scores is same", {
+  expect_equal(base_score, parsedmodel_xgb_reglogistic_basescore$general$params$base_score)
+  expect_equal(base_score, parsedmodel_xgb_binarylogistic_basescore$general$params$base_score)
+})
+
+xgb_reglogistic_basescore_sql <-
+  build_fit_formula_xgb(parsedmodel_xgb_reglogistic_basescore)
+xgb_binarylogistic_basescore_sql <-
+  build_fit_formula_xgb(parsedmodel_xgb_binarylogistic_basescore)
+
+xgb_reglogistic_basescore_pred_model <-
+  predict(xgb_reglogistic_basescore, xgb_bin_data) %>% round(5)
+xgb_binarylogistic_basescore_pred_model <-
+  predict(xgb_binarylogistic_basescore, xgb_bin_data) %>% round(5)
+
+xgb_reglogistic_basescore_pred_sql <-
+  mtcars %>%
+  dplyr::mutate_(yhat_sql = as.character(xgb_reglogistic_basescore_sql)[[3]]) %>%
+  dplyr::mutate(yhat_sql = 1 - yhat_sql) %>%
+  .$yhat_sql %>% 
+  round(5)
+
+xgb_binarylogistic_basescore_pred_sql <-
+  mtcars %>%
+  dplyr::mutate_(yhat_sql = as.character(xgb_binarylogistic_basescore_sql)[[3]]) %>%
+  dplyr::mutate(yhat_sql = 1 - yhat_sql) %>%
+  .$yhat_sql %>% 
+  round(5)
+
+test_that("Same predictions between model and parsed sql model", {
+  expect_equal(xgb_reglogistic_basescore_pred_model, xgb_reglogistic_basescore_pred_sql)
+  expect_equal(xgb_binarylogistic_basescore_pred_model, xgb_binarylogistic_basescore_pred_sql)
+})
