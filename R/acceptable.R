cbd629ad00a490e6b9ffa2e1e4e5af66bd7ba658
Nachiket Soni <60312074+ThenoobMario@users.noreply.github.com>
2020-08-19 15:18:07 +0530

    Edited typos

--- R/acceptable.R
+++ R/acceptable.R
@@ -2,7 +2,7 @@
 #'
 #' Uses an S3 method to check that a given formula can be parsed based on its class.
 #' It currently scans for contrasts that are not supported and in-line functions.
-#' (e.g: lm(wt ~ as.factor(am))). Sine this function is meant for function interaction,
+#' (e.g: lm(wt ~ as.factor(am))). Since this function is meant for function interaction,
 #' as opposed to human interaction, a successful check is silent.
 #'
 #' @param model An R model object
@@ -32,7 +32,8 @@ acceptable_formula.glm <- function(model) {
 fun_calls <- function(f) {
   if (is.function(f)) {
     fun_calls(body(f))
-  } else if (is.call(f)) {
+  } 
+  else if (is.call(f)) {
     fname <- as.character(f[[1]])
     # Calls inside .Internal are special and shouldn't be included
     if (identical(fname, ".Internal")) {
