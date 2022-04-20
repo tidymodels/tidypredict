041f22697429efddc08d05a11d84a2cea831b5b1
Simon P. Couch <simonpatrickcouch@gmail.com>
2022-04-20 10:28:47 -0400

    ignore randomForest on old versions of R

--- tests/testthat/test-list.R
+++ tests/testthat/test-list.R
@@ -2,6 +2,8 @@ context("test-list")
 
 fitted_rf <- function(...) tidypredict_fit(parse_model(...))
 
+skip_if_not_installed("randomForest")
+
 test_that("Supports parsed models in list objects", {
   expect_is(
     fitted_rf(lm(mpg ~ wt, data = mtcars)),
