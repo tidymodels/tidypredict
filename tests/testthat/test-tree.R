bcf9c27bb89ffac65e8881474164eb9730889b55
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2025-11-03 12:36:11 -0800

    reorder

--- tests/testthat/test-tree.R
+++ tests/testthat/test-tree.R
@@ -1,24 +1,3 @@
-test_that("get_rf_case() avoids ifelse if path is always TRUE (#143)", {
-  node <- list(
-    path = list(),
-    prediction = list(
-      list(col = "(Intercept)", val = 37.2, op = "none", is_intercept = 1),
-      list(col = "hp", val = -0.0318, op = "multiply", is_intercept = 0),
-      list(col = "wt", val = -3.88, op = "multiply", is_intercept = 0)
-    )
-  )
-
-  expect_identical(
-    expr_text(generate_tree_node(node, calc_mode = "ifelse")),
-    "37.2 + hp * -0.0318 + wt * -3.88"
-  )
-
-  expect_identical(
-    expr_text(generate_tree_node(node, calc_mode = "")),
-    "37.2 + hp * -0.0318 + wt * -3.88"
-  )
-})
-
 test_that("generate_tree_nodes() works", {
   node <- list(
     path = list(
@@ -130,6 +109,27 @@ test_that("generate_tree_node() works", {
   expect_identical(
     generate_tree_node(node, calc_mode = ""),
     quote(disp > 100 ~ 14)
+  )
+})
+
+test_that("generate_tree_node() avoids ifelse if path is always TRUE (#143)", {
+  node <- list(
+    path = list(),
+    prediction = list(
+      list(col = "(Intercept)", val = 37.2, op = "none", is_intercept = 1),
+      list(col = "hp", val = -0.0318, op = "multiply", is_intercept = 0),
+      list(col = "wt", val = -3.88, op = "multiply", is_intercept = 0)
+    )
+  )
+
+  expect_identical(
+    expr_text(generate_tree_node(node, calc_mode = "ifelse")),
+    "37.2 + hp * -0.0318 + wt * -3.88"
+  )
+
+  expect_identical(
+    expr_text(generate_tree_node(node, calc_mode = "")),
+    "37.2 + hp * -0.0318 + wt * -3.88"
   )
 })
 
