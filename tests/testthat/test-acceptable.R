be662be531faef0eec9118f377cc4787c74a68ea
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-02-18 08:12:08 -0800

    fix bug in lm handling of formula

--- tests/testthat/test-acceptable.R
+++ tests/testthat/test-acceptable.R
@@ -4,3 +4,25 @@ m <- lm(x ~ as.factor(y), df)
 test_that("acceptable_formula fails in expected cases", {
   expect_error(acceptable_formula(m))
 })
+
+test_that("glm family function syntax does not trigger error (#202)", {
+  model <- glm(mpg ~ wt + hp, data = mtcars, family = gaussian())
+  expect_no_error(acceptable_formula(model))
+
+  model <- glm(am ~ wt + hp, data = mtcars, family = binomial())
+  expect_no_error(acceptable_formula(model))
+
+  model <- glm(carb ~ wt + hp, data = mtcars, family = poisson())
+  expect_no_error(acceptable_formula(model))
+})
+
+test_that("glm family string syntax works (#202)", {
+  model <- glm(mpg ~ wt + hp, data = mtcars, family = "gaussian")
+  expect_no_error(acceptable_formula(model))
+
+  model <- glm(am ~ wt + hp, data = mtcars, family = "binomial")
+  expect_no_error(acceptable_formula(model))
+
+  model <- glm(carb ~ wt + hp, data = mtcars, family = "poisson")
+  expect_no_error(acceptable_formula(model))
+})
