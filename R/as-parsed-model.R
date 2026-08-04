27d9fa05ca890a8784ee52280163e0e4fe0195f2
Emil Hvitfeldt <emilhhvitfeldt@gmail.com>
2026-08-04 16:09:57 -0700

    Share the multiclass parsed model skeleton
    Seven parsers built the same list: a model name, version, the
    multiclass_regression type, the multinomial family, the class labels and
    their terms.

--- R/as-parsed-model.R
+++ R/as-parsed-model.R
@@ -13,3 +13,21 @@ as_parsed_model.list <- function(x) {
   class(x) <- c("parsed_model", t, class(x))
   x
 }
+
+# Models whose fit is one linear predictor per class, combined with a softmax.
+new_multiclass_parsed_model <- function(
+  model,
+  classes,
+  class_terms,
+  version = 2
+) {
+  pm <- list()
+  pm$general$model <- model
+  pm$general$version <- version
+  pm$general$type <- "multiclass_regression"
+  pm$general$family <- "multinomial"
+  pm$classes <- classes
+  pm$class_terms <- class_terms
+
+  as_parsed_model(pm)
+}
